0d0a7f731d2ba4b9252f8d9086195d8671dbcab8
Mark Kauffman <markkauffman2000@gmail.com>
2017-04-10 14:43:13 -0700

    user controller, template, and view in place

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,6 @@
 defmodule PhoenixDSK.PageController do
   use PhoenixDSK.Web, :controller
-
+  
   def index(conn, _params) do
     render conn, "index.html"
   end
---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,11 @@
+defmodule PhoenixDSK.UserController do
+  use PhoenixDSK.Web, :controller
+  # See http://www.phoenixframework.org/docs/adding-pages
+  # The core of this action is render conn, "index.html". This tells Phoenix
+  # to find a template called index.html.eex and render it. Phoenix will look
+  # for the template in a directory named after our controller,
+  # so web/templates/user.
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
