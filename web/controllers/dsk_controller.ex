4e9f9e023008814c2684ae13c8c667e508d77f9a
Mark Kauffman <markkauffman2000@gmail.com>
2017-05-05 16:37:08 -0700

    depot is like a repot except its the lms vs a db

--- web/controllers/dsk_controller.ex
+++ web/controllers/dsk_controller.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixDSK.DskController do
   use PhoenixDSK.Web, :controller
+  alias PhoenixDSK.Depot, as: Depot
+
   # See http://www.phoenixframework.org/docs/adding-pages
   # The core of this action is render conn, "index.html". This tells Phoenix
   # to find a template called index.html.eex and render it. Phoenix will look
@@ -7,7 +9,7 @@ defmodule PhoenixDSK.DskController do
   # so web/templates/user.
   def index(conn, _params) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
-    {:ok, dskList} = LearnRestClient.all(fqdn, Learn.Dsk)
+    {:ok, dskList} = Depot.all(fqdn, Learn.Dsk)
     render conn, "index.html", dskList: dskList, fqdn: fqdn
   end
 
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixDSK.UserController do
   use PhoenixDSK.Web, :controller
+  alias PhoenixDSK.Depot, as: Depot
+
   # See http://www.phoenixframework.org/docs/adding-pages
   # The core of this action is render conn, "index.html". This tells Phoenix
   # to find a template called index.html.eex and render it. Phoenix will look
@@ -7,13 +9,13 @@ defmodule PhoenixDSK.UserController do
   # so web/templates/user.
   def index(conn, _params) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
-    {:ok, userList} = LearnRestClient.all(fqdn, Learn.User)
+    {:ok, userList} = Depot.all(fqdn, Learn.User)
     render conn, "index.html", userList: userList, fqdn: fqdn
   end
 
   def show(conn, %{"userName" => userName}) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
-    {:ok, user} = LearnRestClient.get(fqdn, Learn.User, userName)
+    {:ok, user} = Depot.get(fqdn, Learn.User, userName)
     render conn, "show.html", user: user
   end
 
