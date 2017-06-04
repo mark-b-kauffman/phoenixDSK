80fc1cd97fac0f016123a364191cd51e38c6f772
Mark Kauffman <markkauffman2000@gmail.com>
2017-06-04 15:35:52 -0700

    cleaning up comments and doc

--- web/controllers/dsk_controller.ex
+++ web/controllers/dsk_controller.ex
@@ -1,12 +1,13 @@
 defmodule PhoenixDSK.DskController do
   use PhoenixDSK.Web, :controller
   alias PhoenixDSK.Lms, as: Lms
-
-  # See http://www.phoenixframework.org/docs/adding-pages
-  # The core of this action is render conn, "index.html". This tells Phoenix
-  # to find a template called index.html.eex and render it. Phoenix will look
-  # for the template in a directory named after our controller,
-  # so web/templates/user.
+  @doc """
+  See http://www.phoenixframework.org/docs/adding-pages
+  The core of this action is render conn, "index.html". This tells Phoenix
+  to find a template called index.html.eex and render it. Phoenix will look
+  for the template in a directory named after our controller,
+  so web/templates/dsk.
+  """
   def index(conn, _params) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
     {:ok, dskList} = Lms.all(fqdn, Learn.Dsk)
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -40,8 +40,8 @@ defmodule PhoenixDSK.UserController do
   """
   def index(conn, _params) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
-    {:ok, userList} = Lms.all(fqdn, Learn.User)
-    {:ok, intentionally, dskMap } = LearnRestClient.get_data_sources(fqdn)
+    {:ok, userList} = Lms.all(fqdn, Learn.User) # List of structs
+    {:ok, intentionallyUnused, dskMap } = LearnRestClient.get_data_sources(fqdn)
     render conn, "index.html", userList: userList, dskMap: dskMap, fqdn: fqdn
   end
 
@@ -51,7 +51,7 @@ defmodule PhoenixDSK.UserController do
   def show(conn, %{"userName" => userName}) do
     fqdn = Application.get_env(:phoenixDSK, PhoenixDSK.Endpoint)[:learnserver]
     {:ok, user} = Lms.get(fqdn, Learn.User, userName) # user as struct
-    {:ok, intentionally, dskMap} = LearnRestClient.get_data_sources(fqdn)
+    {:ok, intentionallyUnused, dskMap} = LearnRestClient.get_data_sources(fqdn)
     # dskMap =  LearnRestClient.get(String.to_atom(fqdn), "dskMap")
     # dskList = [%{"id" => "_2_1", "externalId" => "SYSTEM"}, %{"id" => "_1_1", "externalId" => "INTERNAL"}]
     # here we need a util method that takes the dskMap and returns a list in the above form....
